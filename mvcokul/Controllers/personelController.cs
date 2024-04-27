af37f2926301d9ecdc247b64048620d368654fe2
hasancnkl <hasancnklc1@gmail.com>
2024-04-27 17:09:25 +0300

    Proje dosyası ekle.

---
+++ mvcokul/Controllers/personelController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcokul.Models.entity;
+
+namespace mvcokul.Controllers
+{
+    public class personelController : Controller
+    {
+        projeEntities db = new projeEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.ogretmen.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult Yenikategori()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Yenikategori(ogretmen p1)
+        {
+            db.ogretmen.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIL(int id)
+        {
+            var Kategori = db.ogretmen.Find(id);
+            db.ogretmen.Remove(Kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.ogretmen.Find(id);
+            return View("KategoriGetir", ktgr);
+        }
+        public ActionResult Guncelle(ogretmen p1)
+        {
+            var ktg = db.ogretmen.Find(p1.ogretid);
+            ktg.ogretmenadi = p1.ogretmenadi;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ mvcokul/Controllers/raporlarController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcokul.Models.entity;
+
+namespace mvcokul.Controllers
+{
+    public class raporlarController : Controller
+    {
+        projeEntities db = new projeEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.ogretmen.ToList();
+            var degerler1= db.ogrenci.ToList();
+            return View(degerler);
+            return View(degerler1);
+        }
+
+        [HttpGet]
+        public ActionResult Yenikategori()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Yenikategori(ogrenci p1)
+        {
+            db.ogrenci.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIL(int id)
+        {
+            var Kategori = db.ogrenci.Find(id);
+            db.ogrenci.Remove(Kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.ogrenci.Find(id);
+            return View("KategoriGetir", ktgr);
+        }
+        public ActionResult Guncelle(ogrenci p1)
+        {
+            var ktg = db.ogrenci.Find(p1.ogrid);
+            ktg.ogradi = p1.ogradi;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
